909306f374e49229a1152b20a51eec127ee7784c
tqueguin <tanguyqueguineur@gmail.com>
2023-05-14 19:42:43 +0200

    change labo

--- labo.c
+++ labo.c
@@ -1,27 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+
+#include "messages.h"
 #include "utils_v2.h"
-#include <unistd.h>
 
-#define NUMBER_OF_ZOMBIES 2
 
-void launchZombie(){
-   sexecl("./zombie", "zombie", NULL);
+
+void childhandler(int port)
+{
+   char *args[]={"./zombie",port,NULL};
+   execvp(args[0],args);
 }
 
-int main() {
-   int zombies[NUMBER_OF_ZOMBIES];
-   int c;
-   for(int i = 0; i<NUMBER_OF_ZOMBIES;i++){
-         zombies[i] = fork_and_run0(launchZombie);
-   }
-   while ((c = getchar()) != EOF) {
-      sleep(1);
-   }
+int main(int argc, char **argv)
+{
 
-   for(int i = 0; i<NUMBER_OF_ZOMBIES; i++){
-      skill(zombies[i], SIGINT);
-   }
+
+
+int possiblePorts[10] = {SERVER_PORT_1, SERVER_PORT_2, SERVER_PORT_3, SERVER_PORT_4, 
+        SERVER_PORT_5, SERVER_PORT_6, SERVER_PORT_7, SERVER_PORT_8,
+        SERVER_PORT_9, SERVER_PORT_10};
+
+int tab_size = 10;
+
+srand(time(NULL));
+
+
+int port_pris = -1;
+for(int i = 0;i<2;i++){
+
+
+    rand_port = possiblePorts[rand() % tab_size];
+      while(port_pris = rand_port){
+         rand_port = possiblePorts[rand() % tab_size];
+      }
+    port_pris = rand_port;
+    pid_t childID = fork_and_run0(childhandler,rand_port);
+    kill(childID, NULL);
    
-   return 0;
-}
 
+ }
+   
+}
